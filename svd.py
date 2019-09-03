404531f7ed480dbdc049c75e1ac0907d1f9b78cc
prikol <atrd.konstantin@gmail.com>
2019-09-03 22:11:23 +0300

    Update commit

---
+++ svd.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def delta_svd(A, delta):
+    U, S, Vt = np.linalg.svd(A, full_matrices=False)
+    rank = len(S)
+    while rank > 0 and np.linalg.norm(A - U[:, :rank].dot(np.diag(S[:rank])).dot(Vt[:rank, :])) <= delta:
+        rank -= 1
+    return U[:, :rank + 1], S[:rank + 1], Vt[:rank + 1, :]
+
+
+def TT_SVD(A, eps):
+    delta = eps / (np.sqrt(len(A.shape) - 1)) * np.linalg.norm(A)
+    C = np.copy(A)
+    r_prev, r_cur = 1, 0
+    cores = []
+
+    for k in range(1, len(A.shape)):
+        C = np.reshape(C, (r_prev * A.shape[k], C.size // (r_prev * A.shape[k])))
+        U, S, Vt = delta_svd(C, delta)
+        r_cur = len(S)
+        G_k = np.reshape(U, (r_prev, A.shape[k], r_cur))
+
+        cores.append(G_k)
+        C = S.dot(Vt)
+        r_prev = r_cur
+
+    cores.append(C)
+    return cores
+
+
+a = np.array([[[1, 2], [3, 4]],
+              [[5, 6], [7, 8]]])
+
+cores = TT_SVD(a, 1)
+print(*cores, sep="\n\n\n\n")
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print("Hello world")
-print("nonono")
-print("yezee")
+def bin_search(a, target):
+    low, high = 0, len(a)
+    while low < high:
+        mid = (low + high) // 2
+        if a[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+    return mid
+
+
+bin_search([i for i in range(100)], 6)
